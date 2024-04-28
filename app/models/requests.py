e2895ab7428cfa10b216bc27eef585e56d67fa81
Caio <cmarchiorato@hotmail.com>
2024-04-28 20:04:13 -0300

    requests

---
+++ app/models/requests.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from app import db, ma
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+class Farm(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+class Requests(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_client = db.Column(db.Integer, db.ForeignKey('client'), nullable=False)
+    id_farm = db.Column(db.Integer, db.ForeignKey('farm'), nullable=False)
+    id_user = db.Column(db.Integer, db.ForeignKey('user'), nullable=False)
+    id_product = db.Column(db.Integer, db.ForeignKey('product'), nullable=False)
+    product_quantity = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.String(10), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    last_modified = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
+
+def __init__(self, id_client, id_farm, id_user, id_product, product_quantity, date, status):
+    self.id_client = id_client
+    self.id_farm = id_farm
+    self.id_user = id_user
+    self.id_product = id_product
+    self.product_quantity = product_quantity
+    self.date = date
+    self.status = status
+
+class RequestsSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'id_client', 'id_farm', 'id_user', 'id_product', 'product_quantity', 'date', 'status', 'created_at', 'last_modified')
+
+return_schema = RequestsSchema()
+returns_schema = RequestsSchema(many=True)  #o parametro many=True é usado quando queremos retornar mais de um registro (Um array)
